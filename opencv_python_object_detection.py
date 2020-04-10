0c660e6de6bf5041e36f6b4e36072bdbc74bfbcc
Jet <jet_pham@student.davincischools.rg>
2020-04-09 21:21:37 -0700

    Starting the PolyMorphic outline & labeling the code's structure

--- opencv_python_object_detection.py
+++ opencv_python_object_detection.py
@@ -10,14 +10,22 @@ cap = cv2.VideoCapture(0);
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
-
+# tracking window will display a way to interact with values
 cv2.namedWindow("Tracking")
+# The 'L' Prefix means 'Lower' & the 'U' prefix means 'Upper' these upper and lower value limits determine in what
+# range can a pixels' values can be 'H' is Hue. Hue is the pure color value of a pixel 'S' is saturation. Saturation
+# is the intensity of the pixel's color 'V' is value. Value is the darkness of a pixel you can alter these filters to
+# allow only certain things through the frame & onto the mask the mask the is put metaphorically on top of the frame
+# and lets through the pixel underneath it. to test this out some more, just run this and alter the 'Tracking' window
+# and monitor the 'mask' and 'res' windows in comparison to the 'frame' window to see how they are different and how
+# they change with different track-bar values
 cv2.createTrackbar('LH', 'Tracking', 0, 255, nothing)
 cv2.createTrackbar('LS', 'Tracking', 0, 255, nothing)
 cv2.createTrackbar('LV', 'Tracking', 0, 255, nothing)
 cv2.createTrackbar('UH', 'Tracking', 255, 255, nothing)
 cv2.createTrackbar('US', 'Tracking', 255, 255, nothing)
 cv2.createTrackbar('UV', 'Tracking', 255, 255, nothing)
+cv2.createTrackbar('activate', 'Tracking', 0, 1, nothing)
 
 while cap.isOpened():
 
@@ -32,6 +40,7 @@ while cap.isOpened():
     u_h = cv2.getTrackbarPos('UH', 'Tracking')
     u_s = cv2.getTrackbarPos('US', 'Tracking')
     u_v = cv2.getTrackbarPos('UV', 'Tracking')
+    onSwitch = cv2.getTrackbarPos('activate', 'Tracking')
 
     l_b = np.array([l_h, l_s, l_v])
     u_b = np.array([u_h, u_s, u_v])
@@ -48,15 +57,28 @@ while cap.isOpened():
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
+    # for contour in contours:
+    #     (x, y, w, h) = cv2.boundingRect(contour)
+    #
+    #     if cv2.contourArea(contour) < 10 or cv2.contourArea(contour) > 1150:
+    #         continue
+    #     cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #     cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #     cv2.putText(res, '('+str(int(x+(w/2)))+', '+str(int(y+(h/2)))+')', (int(x+(w/2)), int(y+(h/2))), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+    #     cv2.putText(res, 'Status: {}'.format('movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    if onSwitch == 1:
+        gray1 = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 
-        if cv2.contourArea(contour) < 1500:
-            continue
-        cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(res, '('+str(int(x+(w/2)))+', '+str(int(y+(h/2)))+')', (int(x+(w/2)), int(y+(h/2))), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-        cv2.putText(res, 'Status: {}'.format('movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        corners = cv2.goodFeaturesToTrack(gray1, 100, 0.01, 10)
+
+        corners = np.int0(corners)
+
+        for i in corners:
+            x, y = i.ravel()
+            pts = np.array([[x, y]], np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            res = cv2.polylines(res, [pts], True, (0, 255, 255))
+
     cv2.imshow("frame", frame1)
     cv2.imshow("mask", mask)
     cv2.imshow("res", res)
