4cb0e97f51b4132bc025cb8d2f55779102b354b5
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-10-27 18:37:46 -0700

    Initial Intake Vision Code

---
+++ main.py
@@ -0,0 +1,9 @@
+# Import OpenCV libraries
+# Import NetworkTables libraries
+
+# Return of powercells seen by the camera
+def count_powercells():
+
+
+# Send the number of powercells to the robot
+def networktables_send(powercell_count):
