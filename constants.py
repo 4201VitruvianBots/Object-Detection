dd1346005e4f0f93e76f5fd1c805242dde0cd12e
xDanielRamosx <71991614+xDanielRamosx@users.noreply.github.com>
2020-10-27 19:54:18 -0700

    Create constants.py
    Defined all variables

---
+++ constants.py
@@ -0,0 +1,28 @@
+#import magical mystic dragon realm of knowledge (CV)
+def camerainput ():
+    int brightness = 50
+    int contrast = 50
+    int saturation = 50
+    int exposure = 50
+def HSVrange ():
+    int hue = 10
+    for sat in range(45 , 52):
+    for val in range(89 , 255):
+def erode ():
+    int size = 3
+def dilate ():
+    int size = 4
+def grayscale ():
+    placeholder
+def FindCircles():
+    int resolution_divisor = 1
+    int min_distance = 120
+    int edge_detection_threshold = 12
+    int circle_detection_threshold = 10
+    int min_radius = 10
+    int max_radius = 90
+def DrawCircles ():
+    placeholder again
+def CameraServer ():
+    str name = Camera
+    str name = MJPEG
