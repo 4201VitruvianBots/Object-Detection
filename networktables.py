db71397df58371c640d014cac14751227e086895
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-11-01 15:44:23 -0800

    NetworkTables Code Outline

---
+++ networktables.py
@@ -0,0 +1,8 @@
+from networkTables import NetworkTables
+
+# Setup NetworkTables
+NetworkTables.initialize()
+vision_table = NetworkTables.getTable("intake-vision")
+
+# Send value through NetworkTables
+vision_table.putNumber("powercell_count", number);
