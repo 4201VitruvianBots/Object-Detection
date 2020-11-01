762dec2b336b23f78105a65b27d8bf05eef63510
xDanielRamosx <71991614+xDanielRamosx@users.noreply.github.com>
2020-11-01 15:35:33 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,28 +3,28 @@ import numpy as np
 
 def camerainput ():
     brightness = 50
-    int contrast = 50
-    int saturation = 50
-    int exposure = 50
+    contrast = 50
+    saturation = 50
+    exposure = 50
 def HSVrange ():
-    int hue = 10
-    for sat in range(45 , 52):
-    for val in range(89 , 255):
+    hue = 10
+    sat = 48
+    val = 160
 def erode ():
-    int size = 3
+    size = 3
 def dilate ():
-    int size = 4
+    size = 4
 def grayscale ():
-    placeholder
+    placeholder = 0
 def FindCircles():
-    int resolution_divisor = 1
-    int min_distance = 120
-    int edge_detection_threshold = 12
-    int circle_detection_threshold = 10
-    int min_radius = 10
-    int max_radius = 90
+    resolution_divisor = 1
+    min_distance = 120
+    edge_detection_threshold = 12
+    circle_detection_threshold = 10
+    min_radius = 10
+    max_radius = 90
 def DrawCircles ():
-    placeholder again
+    placeholder = 2
 def CameraServer ():
-    str name = Camera
-    str name = MJPEG
+    name = Camera
+    name = MJPEG
