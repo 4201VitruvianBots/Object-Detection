47f376e4d03f44cf9ec693591306568208dea194
Mason-Beard <66457125+Mason-Beard@users.noreply.github.com>
2020-11-01 16:59:57 -0800

    added neworktables

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def count_powercells():
 
 
 # Send the number of powercells to the robot
+networktablesetup 
 def networktables_send(powercell_count):
+    vision_table.putNumber("powercell_count", powercell_count)  
--- networktables.py
+++ networktables.py
@@ -5,4 +5,4 @@ NetworkTables.initialize()
 vision_table = NetworkTables.getTable("intake-vision")
 
 # Send value through NetworkTables
-vision_table.putNumber("powercell_count", number);
+vision_table.putNumber("powercell_count", number)
