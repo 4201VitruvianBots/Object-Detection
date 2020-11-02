e7b8f07e42e4550097af5eefcc7197aa8e16923a
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-11-01 16:05:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 # Import OpenCV libraries
 # Import NetworkTables libraries
+from networktables import NetworkTables
+
+def main():
+    # Setup NetworkTables
+
+    while count_powercells() != -1:
+        # Count the powercells
+
+        # Send the data through NetworkTables
+
 
 # Return of powercells seen by the camera
 def count_powercells():
 
 
 # Send the number of powercells to the robot
-networktablesetup 
-def networktables_send(powercell_count):
-    vision_table.putNumber("powercell_count", powercell_count)  
+def networktables_send(vision_table, powercell_count):
+    vision_table.putNumber("powercell_count", powercell_count)
+
+# Setup NetworkTables
+def networktables_setup():
+    NetworkTables.initialize()
+    vision_table = NetworkTables.getTable("intake-vision")
+    return vision_table
