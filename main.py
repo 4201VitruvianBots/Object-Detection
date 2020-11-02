c221ffbeceb98f8f5b2b69a80636644940b645f1
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-11-01 16:19:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from networktables import NetworkTables
 
 def main():
     # Setup NetworkTables
+    
+    
+    while count_powercells() != -1:
+        # Count the powercells
+        count_powercells()
+
+        # Send the data through NetworkTables
+        networktables_send(vision_table, count)
+
+def networktables_send(vision_table, powercell_count):
+    vision_table.putNumber("powercell_count", powercell_count)
+
+
 def networktables_setup():
     NetworkTables.initialize()
     vision_table = NetworkTables.getTable("intake-vision")
     return vision_table
-    
-    while count_powercells() != -1:
-        # Count the powercells
-    def count_powercells():
-
-        # Send the data through NetworkTables
-def networktables_send(vision_table, powercell_count):
-    vision_table.putNumber("powercell_count", powercell_count)
-
 
 # Return of powercells seen by the camera
 def networktables_return():
