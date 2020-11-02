12e178235946f86ed7d67d10921243f6ebb12dc2
Mason-Beard <66457125+Mason-Beard@users.noreply.github.com>
2020-11-01 17:13:12 -0800

    Reorganized

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from networktables import NetworkTables
 
 def main():
     # Setup NetworkTables
-
-    while count_powercells() != -1:
-        # Count the powercells
-
-        # Send the data through NetworkTables
-
-
-# Return of powercells seen by the camera
-def count_powercells():
-
-
-# Send the number of powercells to the robot
-def networktables_send(vision_table, powercell_count):
-    vision_table.putNumber("powercell_count", powercell_count)
-
-# Setup NetworkTables
 def networktables_setup():
     NetworkTables.initialize()
     vision_table = NetworkTables.getTable("intake-vision")
     return vision_table
+    
+    while count_powercells() != -1:
+        # Count the powercells
+    def count_powercells():
+
+        # Send the data through NetworkTables
+def networktables_send(vision_table, powercell_count):
+    vision_table.putNumber("powercell_count", powercell_count)
+
+
+# Return of powercells seen by the camera
+def networktables_return():
+    return powercell
+
+
+
+
+
+
+
