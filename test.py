c2b8c8f593df7b9b7d1261399ec106c80dbeb35a
Sheraz4201 <Sheraz24132@gmail.com>
2020-12-01 19:25:51 -0800

    Added Number

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from main.py import count_powercells
 def main():
    # Run the count_powercells function and get the return value
    # Return value example (delete line below)
-   return_value_variable = function_name()
    # Print out that return value
+   powercellcounter = count_powercells() 
+   print("Number: " + powercellcounter)
    
